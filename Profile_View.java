0b3897a4986112a1d88947fcfbf15e6f3be0b73d
mariya mustary <mariyamustary020@gmail.com>
2026-05-29 12:36:25 +0600

    Today I have made the structure of vier profile window

---
+++ Profile_View.java
@@ -0,0 +1,55 @@
+package profile_view;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Profile_View extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        
+    }
+
+    public static void profile(String name, String phone, String email) {
+
+        // profile window creation
+        Stage profileStage = new Stage();
+
+        // Labels
+        Label title = new Label("View Profile");
+
+        Label nameLabel = new Label(
+                "Name: " + name
+        );
+
+        Label phoneLabel = new Label(
+                "Phone: " + phone
+        );
+
+        Label emailLabel = new Label(
+                "Email: " + email
+        );
+
+        // Layout
+        VBox layout = new VBox(10);
+
+        layout.getChildren().addAll(title,
+                nameLabel,               
+                phoneLabel,
+                emailLabel
+        );
+
+        Scene scene = new Scene(layout, 300, 200);
+
+        // Set scene
+        profileStage.setScene(scene);
+        profileStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
