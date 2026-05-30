8ad9e4ea78a0429ed571e6363865b43dd5a61d50
ofamobashwera98-cyber <ofamobashwera98@gmail.com>
2026-05-30 18:58:40 +0600

    Today i created some saved cotacts also gave the functionality to the to the add contacts button

--- src/phonecontactui1/ContactListPage.java
+++ src/phonecontactui1/ContactListPage.java
@@ -3,49 +3,92 @@ package PhoneContactUI1;
 
 import javafx.application.Application;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 public class ContactListPage extends Application {
 
+    VBox contactList = new VBox(15);
+    HBox inputBox;
+
     @Override
-    public void start(Stage primaryStage) { 
-        Label l = new Label("Contact List");
-        BorderPane root = new BorderPane();
-        //layout
-        VBox main = new VBox(20);
-        main.setPadding(new Insets(20));
-        
-        //search bar
+    public void start(Stage primaryStage) {
+
+        VBox layout = new VBox(15);
+        layout.setPadding(new Insets(20));
+
         TextField search = new TextField();
-
         search.setPromptText("Search Contacts");
-
-        search.setPrefHeight(45);
-
+         
         HBox buttonRow = new HBox(10);
-        
-        //all the buttons
+
         Button allBtn = new Button("All");
-        Button Add = new Button("Add Contacts");
-        
-        buttonRow.getChildren().addAll(allBtn,Add);
+        Button addBtn = new Button("Add Contact");
 
-        main.getChildren().addAll(l,search,buttonRow);
+        buttonRow.getChildren().addAll(allBtn, addBtn);
 
-        root.setCenter(main);
+        TextField name = new TextField();
+        name.setPromptText("Name");
 
-        Scene scene = new Scene(root, 450, 750);
+        TextField number = new TextField();
+        number.setPromptText("Number");
 
-        primaryStage.setTitle("Phone Contact UI");
+        inputBox = new HBox(10, name, number);
+        inputBox.setVisible(false);
+
+        // saved contacts
+        addContact("Prapti", "01711111111");
+        addContact("Mariya", "01822222222");
+        addContact("Esha", "01333333333");
+        addContact("Sifat", "01933333323");
+
+        // show the box where i can add name and number
+        addBtn.setOnAction(e -> {
+            if (!layout.getChildren().contains(inputBox)) {
+                layout.getChildren().add(inputBox);
+            }
+            inputBox.setVisible(true);//the input box will pop
+      
+            if (!name.getText().isEmpty() && !number.getText().isEmpty()) {
+
+                addContact(name.getText(), number.getText());
+
+                name.clear();
+                number.clear();
+
+                inputBox.setVisible(false);//afyrt xcontact saving the box will vanish
+            }
+        });
+
+        layout.getChildren().addAll(search, buttonRow, contactList);
+
+        Scene scene = new Scene(layout, 400, 600);
         primaryStage.setScene(scene);
+        primaryStage.setTitle("Contact List");
         primaryStage.show();
+    }
+
+    void addContact(String name, String number) {
+
+        HBox box = new HBox(15);
+        box.setAlignment(Pos.CENTER_LEFT);
+
+        VBox info = new VBox(5);
+        info.getChildren().addAll(new Label(name), new Label(number));
+
+        Region spacer = new Region();
+        HBox.setHgrow(spacer, Priority.ALWAYS);
+
+        Button call = new Button("Call");
+
+        box.getChildren().addAll(info, spacer, call);
+
+        contactList.getChildren().add(box);
     }
 
     public static void main(String[] args) {
